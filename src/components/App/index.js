9fbe279e523496a59c060b5122c5006a6aefbaa5
Dmytro Fidyk <fidykdl@gmail.com>
2022-03-18 12:11:05 +0200

    Modified component Message: added props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,31 @@
 import './styles.css';
 
 import Header from '../Header';
+import Message from '../Message';
 import Form from '../Form';
+
+const messageData = {
+    //id: 1,
+    nickname: 'Дмитро',
+    messageText: 'Вітання з мирної, вільної України!',
+    time: '12:15'
+};
+
+const myMessageData = {
+    //id: 1,
+    nickname: 'Дмитрик',
+    messageText: "Обов'язково будуть Мир і Перемога!",
+    time: '12:15',
+    isMyMessage: true
+};
 
 const App = () => {
     return (
         <div className="main-container">
             <Header/>
             <div className="chat-container">
-                <h1>Вітання з мирної, вільної України!</h1>
+                <Message messageData={messageData} />
+                <Message messageData={myMessageData} />
             </div>          
             <Form/>
         </div>
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,12 +1,20 @@
 import './styles.css';
 
 const Message = (props) => {
+    const { nickname, messageText, time } = props.messageData;
+
+    let my = '';
+    
+    if (props.messageData.isMyMessage) {
+        my = 'my-';
+    }
+
     return (
-        <div className="message-container">
-            <div className="message">
-                <div className="nickname"></div>
-                <div className="message-text"></div>
-                <div className="time"></div>
+        <div className={`${my}message-container`}>
+            <div className={`${my}message`}>
+                <div className="nickname">{nickname}</div>
+                <div className="message-text">{messageText}</div>
+                <div className="time">{time}</div>
             </div>
         </div>
     );
